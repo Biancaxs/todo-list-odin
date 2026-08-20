3e15f6f08ff2f0bd570f6fed037d99b46d73ea95
Bianca Xavier <202794770+Biancaxs@users.noreply.github.com>
2026-08-20 14:53:18 -0300

    initialize core application state and connect modules in index entry point

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log("Webpack funcionando!")
+import "./style.css"
+import { initializeApp } from './appLogic.js'
+import { renderProjects } from './domController.js'
+
+initializeApp()
+renderProjects()
